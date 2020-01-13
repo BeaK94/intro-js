9e89d0d69b934ed17a946ec34012d65bfefb91fe
BeaK94 <beatakusnierz94@gmail.com>
2020-01-13 13:19:47 +0200

    added some test javascript

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+console.log('This is it.');
+
+var a = 1;
+var b = 2;
+var c = a + b;
+
+if (a < b){
+     /* if the condition is met,
+     the following line is executed */
+    console.log('a is lesser than b');
+} else {
+    /*  if condition not met,
+    that line is executed */
+    console.log('a is greater than b');
+}
+
+if (a === 1) {
+    console.log('a is exactly 1');
+};
