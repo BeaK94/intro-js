9d0dcd7d4d5888508a221b8e5ff9ab7185dcbb1c
BeaK94 <beatakusnierz94@gmail.com>
2020-01-27 14:13:30 +0200

    created a slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,83 @@
-(function () {
-    var itemClassName = 'slider__photo';
-        items = document.getElementsByClassName(itemClassName),
-        totalItems = items.length,
-        slide = 0;
+(function() {
+  var itemClassName = "slider__photo";
+  (items = document.getElementsByClassName(itemClassName)),
+    (totalItems = items.length),
+    (slide = 0);
 
-    // Set event listeners
-    function setEventListeners() {
-        var next = document.getElementsByClassName('slider__button--next')[0],
-            prev = document.getElementsByClassName('slider__button--prev')[0];
+  // Set event listeners
+  function setEventListeners() {
+    var next = document.getElementsByClassName("slider__button--next")[0],
+      prev = document.getElementsByClassName("slider__button--prev")[0];
 
-        next.addEventListener('click', getNext);
-        prev.addEventListener('click', getPrev);
+    next.addEventListener("click", getNext);
+    prev.addEventListener("click", getPrev);
+  }
+
+  function getNext() {
+    // TODO
+    //remove "active" from the first img
+    items[slide].classList.remove("active");
+    //change the index of the one I want to be active to the next one of the class of images
+    slide++;
+    if (slide < totalItems) {
+      items[slide].classList.add("active");
+    } else {
+      //if slide is the last picture
+      slide = 0;
+      items[slide].classList.add("active");
     }
+  }
 
-    function getNext() {
-        // TODO
+  /* HOANG'S SOLUTION
+  getNext() {
+      items[slide].classList.remove("active");
+      if (slide === totalItems - 1) {
+          slide = 0;
+      } else {
+          slide++
+      }
+  }
+
+  ANOTHER, WITH A SHORTCUT (instead of the if/else statement)
+  slide = (slide === totalItems - 1 ) ? 0 : slide++;
+  */
+
+  function getPrev() {
+    // TODO
+    items[slide].classList.remove("active");
+    slide--;
+    if (slide >= 0) {
+      items[slide].classList.add("active");
+    } else {
+      //if slide = 0, if the picture is the first one
+      slide = totalItems - 1;
+      items[slide].classList.add("active");
     }
+  }
 
-    function getPrev(){
-        // TODO      
-    }
+  /* HOANG'S SOLUTION
+  getPrev() {
+      items[slide].classList.remove("active");
+      if (slide === 0) {
+          slide = totalItems - 1;
+      } else {
+          slide--
+      }
+  }
 
-    function initSlider() {
-        // setInitialClasses();
-        setEventListeners();
-        
-        // Set moving to false so that the slider becomes interactive
-        moving = false;
-    }
+  ANOTHER, WITH A SHORTCUT (instead of the if/else statement)
+  slide = (slide === 0 ) ? totalItems - 1 : slide++;
 
-    initSlider();
+  not sure if the getPrev works, I wrote it myself based on Hoang's
+  */
+
+  function initSlider() {
+    // setInitialClasses();
+    setEventListeners();
+
+    // Set moving to false so that the slider becomes interactive
+    moving = false;
+  }
+
+  initSlider();
 })();
