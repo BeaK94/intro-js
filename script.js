af94a0f6070fae785c4284960f549cb034712c08
BeaK94 <beatakusnierz94@gmail.com>
2020-02-06 10:37:27 +0200

    Some easier solutions to jQuery tasks

--- script.js
+++ script.js
@@ -28,26 +28,38 @@ $(document).ready(function() {
   $('[data-type="proteinbar"]').css("background-color", "blue");
 
   //adding printing button to both pages, a button that prints the page
-  $(".navbar").append(
-    $('<li><button id="print-btn"></button></li>')
-  );
+  $(".navbar").append($('<li><button id="print-btn"></button></li>'));
   $("#print-btn").on("click", function() {
     window.print();
   });
 
-  
-//making the first word of all person-card paragraphs
+  //making the first word of all person-card paragraphs
 
-//$.each($('p.card-text'), function() {$('p.card-text').text().slice(0,1).css("font-weight", "bold");}); --- doesnt work. The line till /slice)0,1) returns a string of the first LETTER of the paragraph I want
-//$('.card-text').slice(0,1).css("font-weight", "bold")
-//split(' ')[0].css("font-weight", "bold")
+  //$.each($('p.card-text'), function() {$('p.card-text').text().slice(0,1).css("font-weight", "bold");}); --- doesnt work. The line till /slice)0,1) returns a string of the first LETTER of the paragraph I want
+  //$('.card-text').slice(0,1).css("font-weight", "bold")
+  //split(' ')[0].css("font-weight", "bold")
 
+  $(".person-card p").each(function() {
+    var html = $(this).html();
+    var word = html.substr(0, html.indexOf(" "));
+    var rest = html.substr(html.indexOf(" "));
+    $(this)
+      .html(rest)
+      .prepend($("<strong/>").html(word));
+  });
 
-//moving Michael before Henry. Add IDs to Michael and Henry and then insertBefore()
-$('.person-card').last().attr("id", "michael")
-$('.person-card').first().attr("id", "henry")
-$('#michael').insertBefore($('#henry'))
+  //moving Michael before Henry. Add IDs to Michael and Henry and then insertBefore()
+  /* $(".person-card")
+    .last()
+    .attr("id", "michael");
+  $(".person-card")
+    .first()
+    .attr("id", "henry");
+  $("#michael").insertBefore($("#henry")); */
 
-//changing "people" headline and "people" in navbar into "personnel"
-$('#people h2, [href="#people"]').html("personnel");
+  //moving Michael before Henry. Searching for the name!
+  $(".person-card:contains(Lewiston)").prependTo(".people-cards");
+
+  //changing "people" headline and "people" in navbar into "personnel"
+  $('#people h2, [href="#people"]').html("personnel");
 });
